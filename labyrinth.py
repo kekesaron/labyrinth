217d4d8218896f5a9912ccd87ef036feca57ba06
matetotesi <mate.totesi@gmail.com>
2016-05-18 15:29:29 +0200

    Add files via upload

--- labyrinth.py
+++ labyrinth.py
@@ -2,6 +2,7 @@ import curses
 import time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 
+
 def load_map():
     fo = open("map.txt", "r+")
     string=fo.read()
@@ -13,11 +14,36 @@ def open_window(win,string):
     curses.curs_set(0)          # Hiding cursor visibility (https://docs.python.org/2/library/curses.html#curses.curs_set)
     win.keypad(1)               # enable processing of functional keys by curses (ex. arrow keys)              # set a border for the window
     win.nodelay(1)
+    y=1
+    x=1
+    win.addch(y,x, 'O')
+    win.addstr(0,0,string)
     while True:
-        win.addstr(0,0,string)
-        event=win.getch()
-        if (event == ord('q')):
+        event = win.getch()
+        if (event == ord("q")):
             break
+        elif event == curses.KEY_RIGHT :
+            win.addch(y,x, ' ')
+            win.addch(y,x+1, 'O')
+            x=x+1
+            win.refresh()
+        elif event == curses.KEY_LEFT :
+            win.addch(y,x, ' ')
+            win.addch(y,x-1, 'O')
+            x=x-1
+            win.refresh()
+        elif event == curses.KEY_DOWN :
+            win.addch(y,x, ' ')
+            win.addch(y+1,x, 'O')
+            y=y+1
+            win.refresh()
+        elif event == curses.KEY_UP :
+            win.addch(y,x, ' ')
+            win.addch(y-1,x, 'O')
+            y=y-1
+            win.refresh()
+
+
     curses.endwin()
 win= curses.initscr()
 load_map()
